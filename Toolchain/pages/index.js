04f555c6667045c48acf687a87941e3d8e719ef1
Tenzin.T <tseringtenzin960@gmail.com>
2024-10-02 10:40:13 +0200

    Toolchain

---
+++ Toolchain/pages/index.js
@@ -0,0 +1,10 @@
+import style from "./index.module.css";
+
+export default function IndexPage() {
+  return (
+    <div className={style.index}>
+      <header></header>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
